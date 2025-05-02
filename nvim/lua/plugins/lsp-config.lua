9b9e1630e0d05a11b3a3368bace37e8ade9e75c9
Unmystic <unmystic7@gmail.com>
2025-05-02 12:59:37 +0200

    updated js html lynting

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "ruff", "biome", "eslint", "ts_ls" },
+                ensure_installed = { "lua_ls", "pyright", "ruff", "biome", "eslint", "ts_ls" ,"cssls"},
             })
         end,
     },
@@ -24,6 +24,7 @@ return {
             -- lspconfig.biome.setup({ capabilities = capabilities })
             -- lspconfig.eslint.setup({ capabilities = capabilities })
             lspconfig.ts_ls.setup({ capabilities = capabilities })
+            lspconfig.cssls.setup({capabilities = capabilities})
 
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
