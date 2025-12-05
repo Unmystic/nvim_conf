24a28d0e4d2bbf9b330c86e829c9dcda99b5bc6f
Unmystic <unmystic7@gmail.com>
2025-12-05 12:43:32 +0100

    Added autoload manual imports for Python

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
-
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build=':TSUpdate',
-    config = function()
-        local config = require('nvim-treesitter.configs')
-        config.setup {
-        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "javascript", "lua", "vim", "python"},
-        auto_install = true,
-        highlight = {enable=true},
-        indent = {enable=true},
-        }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+			ensure_installed = { "javascript", "lua", "vim", "python" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		vim.treesitter.language.register("html", "ejs")
+		vim.treesitter.language.register("javascript", "ejs")
+	end,
 }
